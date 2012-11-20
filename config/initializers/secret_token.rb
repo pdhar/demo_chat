2221228f81301122da48a649053f5a986bbbbabc
Pranav Dhar <pranav.dhar2@gmail.com>
2012-11-20 23:57:33 +0400

    New Chat Demo App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoChat::Application.config.secret_token = '86978d5ba3f8856f53a7956b485a116f870f77a5c11314a4f11fb1b304d840fccb60872fe06d40c192974a98dfc2fc3fb01e6ca07d71cb929ce413317da851a2'
